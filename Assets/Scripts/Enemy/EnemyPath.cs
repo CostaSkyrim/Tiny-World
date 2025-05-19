0973bc925d8103c605078177a222b0b4bb404492
Christos Markos <xristosssmarkosss@gmail.com>
2025-05-20 02:16:22 +0300

    enemy path

--- Assets/Scripts/Enemy/EnemyPath.cs
+++ Assets/Scripts/Enemy/EnemyPath.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class EnemyPath : MonoBehaviour
 {
     public Transform[] patrolPoints;
-    public float speed;
+    public float speed = 21f;
+    public float rotationSpeed = 2f;
     public int targetPoint;
+    private Quaternion targetRotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +20,28 @@ public class EnemyPath : MonoBehaviour
     {
         if (transform.position == patrolPoints[targetPoint].position)
         {
+            stop();
             changeTarget();
+            Invoke("start", 1f);
         }
         Transform target = patrolPoints[targetPoint];
         transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 
 
     void changeTarget()
     {
         targetPoint = (targetPoint + 1) % patrolPoints.Length;
+        Vector3 directionToTarget = patrolPoints[targetPoint].position - transform.position;
+        targetRotation = Quaternion.LookRotation(directionToTarget);
+    }
+    void stop()
+    {
+        speed = 0;
+    }
+    void start()
+    {
+        speed = 21f;
     }
 }
