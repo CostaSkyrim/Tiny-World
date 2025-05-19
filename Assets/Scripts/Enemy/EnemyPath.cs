4bdbc0437bd73166de35061d51d6046fed2b6ca1
Christos Markos <xristosssmarkosss@gmail.com>
2025-05-20 02:31:57 +0300

    FIX(enemy path):better varables

--- Assets/Scripts/Enemy/EnemyPath.cs
+++ Assets/Scripts/Enemy/EnemyPath.cs
@@ -6,13 +6,16 @@ public class EnemyPath : MonoBehaviour
 {
     public Transform[] patrolPoints;
     public float speed = 21f;
-    public float rotationSpeed = 2f;
-    public int targetPoint;
+    public float stopTime = 1.2f;
+    public float rotationSpeed = 2.5f;
+    private int targetPoint;
+    private float enemySpeed;
     private Quaternion targetRotation;
     // Start is called before the first frame update
     void Start()
     {
         targetPoint = 0;
+        enemySpeed = speed;
     }
 
     // Update is called once per frame
@@ -22,10 +25,10 @@ public class EnemyPath : MonoBehaviour
         {
             stop();
             changeTarget();
-            Invoke("start", 1f);
+            Invoke("start", stopTime);
         }
         Transform target = patrolPoints[targetPoint];
-        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, target.position, enemySpeed * Time.deltaTime);
         transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 
@@ -38,10 +41,10 @@ public class EnemyPath : MonoBehaviour
     }
     void stop()
     {
-        speed = 0;
+        enemySpeed = 0;
     }
     void start()
     {
-        speed = 21f;
+        enemySpeed = speed;
     }
 }
