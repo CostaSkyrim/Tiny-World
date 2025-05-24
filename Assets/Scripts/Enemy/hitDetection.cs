993a5a6a1c697c9f833bbbf3f57af4bfbd58108b
Kostolzin <104931991+Kostolzin@users.noreply.github.com>
2025-05-24 18:46:38 +0300

    cutscene timeline

--- Assets/Scripts/Enemy/hitDetection.cs
+++ Assets/Scripts/Enemy/hitDetection.cs
@@ -5,17 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class hitDetection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+   
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
--- Assets/TriggerTest.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class TriggerTest : MonoBehaviour
-{
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Trigger entered by: " + other.name);
-    }
-}
